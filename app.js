25acef0fb8583c38e574b202d4e2737e92125b53
Hammam Irfantoro <hirfantoro@yahoo.com>
2020-02-01 16:05:21 +0700

    Second Commit

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const loginroutes = require('./routes/login');
-
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(bodyParser.json());
+const mongoose = require('mongoose');
 
 const PORT = 3000;
 
 
+const app = express();
 
+//connect to Mongoose
 
-app.use('login',loginroutes);
-app.get('/',(req,res,next)=>{
-    res.send('hello exoress!');
+// mongoose.connect('mongodb://localhost/rekatani-dev', {
+//     useMongoclient: true
+// }).then(()=> console.log('MongoDB Connected...'))
+// .catch(err=> console.log(err));
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+//test route
+app.get('/',(req,res,next)=>{
+    res.send('hello express!');
+});
+
+//routes to handle user registration
+app.use('/auth',loginroutes);
+
 
 
 
